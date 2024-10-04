520a2d3a7b2ab534a655608592e7095f2cc68411
LuciferDIot <105873753+LuciferDIot@users.noreply.github.com>
2024-10-04 16:52:23 +0530

    hydration error fixed

--- stores/eventsStore.ts
+++ stores/eventsStore.ts
@@ -6,6 +6,7 @@ import { persist } from "zustand/middleware";
 interface EventStore {
   events: IEvent[];
   setEvents: (events: IEvent[]) => void;
+  hasHydrated: boolean; // Add a hydration flag
 }
 
 export const useEventStore = create<EventStore>()(
@@ -13,9 +14,14 @@ export const useEventStore = create<EventStore>()(
     (set) => ({
       events: [],
       setEvents: (events) => set({ events }),
+      hasHydrated: false, // Initialize the hydration flag
     }),
     {
       name: "event-storage", // Unique name for the storage
+      onRehydrateStorage: () => (state) => {
+        // Set hasHydrated to true after rehydration
+        if (state) state.hasHydrated = true;
+      },
     }
   )
 );
